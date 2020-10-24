9276987095ba03616ecf5485bc6555a8a3b3257c
Igor Santos <igor.siqueira12@gmail.com>
2020-10-24 15:45:50 -0300

    reconheceu

--- Src/stm32l0xx_it.c
+++ Src/stm32l0xx_it.c
@@ -1,27 +1,28 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    stm32l0xx_it.c
-  * @brief   Interrupt Service Routines.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    stm32l0xx_it.c
+ * @brief   Interrupt Service Routines.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
 #include <stdio.h>
 #include "main.h"
 #include "stm32l0xx_it.h"
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 /* USER CODE END Includes */
@@ -33,7 +34,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
- 
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -58,8 +59,9 @@
 
 /* External variables --------------------------------------------------------*/
 extern UART_HandleTypeDef huart1;
-//extern RTC_HandleTypeDef hrtc;
-//extern UART_HandleTypeDef huart2;
+extern char rx_buff[32];
+extern char rx_data[32];
+
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -68,71 +70,71 @@ extern UART_HandleTypeDef huart1;
 /*           Cortex-M0+ Processor Interruption and Exception Handlers          */ 
 /******************************************************************************/
 /**
-  * @brief This function handles Non maskable Interrupt.
-  */
+ * @brief This function handles Non maskable Interrupt.
+ */
 void NMI_Handler(void)
 {
-  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+	/* USER CODE BEGIN NonMaskableInt_IRQn 0 */
 
-  /* USER CODE END NonMaskableInt_IRQn 0 */
-  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+	/* USER CODE END NonMaskableInt_IRQn 0 */
+	/* USER CODE BEGIN NonMaskableInt_IRQn 1 */
 
-  /* USER CODE END NonMaskableInt_IRQn 1 */
+	/* USER CODE END NonMaskableInt_IRQn 1 */
 }
 
 /**
-  * @brief This function handles Hard fault interrupt.
-  */
+ * @brief This function handles Hard fault interrupt.
+ */
 void HardFault_Handler(void)
 {
-  /* USER CODE BEGIN HardFault_IRQn 0 */
+	/* USER CODE BEGIN HardFault_IRQn 0 */
 
-  /* USER CODE END HardFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
-    /* USER CODE END W1_HardFault_IRQn 0 */
-  }
+	/* USER CODE END HardFault_IRQn 0 */
+	while (1)
+	{
+		/* USER CODE BEGIN W1_HardFault_IRQn 0 */
+		/* USER CODE END W1_HardFault_IRQn 0 */
+	}
 }
 
 /**
-  * @brief This function handles System service call via SWI instruction.
-  */
+ * @brief This function handles System service call via SWI instruction.
+ */
 void SVC_Handler(void)
 {
-  /* USER CODE BEGIN SVC_IRQn 0 */
+	/* USER CODE BEGIN SVC_IRQn 0 */
 
-  /* USER CODE END SVC_IRQn 0 */
-  /* USER CODE BEGIN SVC_IRQn 1 */
+	/* USER CODE END SVC_IRQn 0 */
+	/* USER CODE BEGIN SVC_IRQn 1 */
 
-  /* USER CODE END SVC_IRQn 1 */
+	/* USER CODE END SVC_IRQn 1 */
 }
 
 /**
-  * @brief This function handles Pendable request for system service.
-  */
+ * @brief This function handles Pendable request for system service.
+ */
 void PendSV_Handler(void)
 {
-  /* USER CODE BEGIN PendSV_IRQn 0 */
+	/* USER CODE BEGIN PendSV_IRQn 0 */
 
-  /* USER CODE END PendSV_IRQn 0 */
-  /* USER CODE BEGIN PendSV_IRQn 1 */
+	/* USER CODE END PendSV_IRQn 0 */
+	/* USER CODE BEGIN PendSV_IRQn 1 */
 
-  /* USER CODE END PendSV_IRQn 1 */
+	/* USER CODE END PendSV_IRQn 1 */
 }
 
 /**
-  * @brief This function handles System tick timer.
-  */
+ * @brief This function handles System tick timer.
+ */
 //void SysTick_Handler(void)
 //{
-  /* USER CODE BEGIN SysTick_IRQn 0 */
+/* USER CODE BEGIN SysTick_IRQn 0 */
 
-  /* USER CODE END SysTick_IRQn 0 */
+/* USER CODE END SysTick_IRQn 0 */
 //  HAL_IncTick();
-  /* USER CODE BEGIN SysTick_IRQn 1 */
+/* USER CODE BEGIN SysTick_IRQn 1 */
 
-  /* USER CODE END SysTick_IRQn 1 */
+/* USER CODE END SysTick_IRQn 1 */
 //}
 
 /******************************************************************************/
@@ -143,84 +145,92 @@ void PendSV_Handler(void)
 /******************************************************************************/
 
 /**
-  * @brief This function handles RTC global interrupt through EXTI lines 17, 19 and 20 and LSE CSS interrupt through EXTI line 19.
-  */
+ * @brief This function handles RTC global interrupt through EXTI lines 17, 19 and 20 and LSE CSS interrupt through EXTI line 19.
+ */
 //void RTC_IRQHandler(void)
 //{
-  /* USER CODE BEGIN RTC_IRQn 0 */
+/* USER CODE BEGIN RTC_IRQn 0 */
 
-  /* USER CODE END RTC_IRQn 0 */
+/* USER CODE END RTC_IRQn 0 */
 //  HAL_RTC_AlarmIRQHandler(&hrtc);
-  /* USER CODE BEGIN RTC_IRQn 1 */
+/* USER CODE BEGIN RTC_IRQn 1 */
 
-  /* USER CODE END RTC_IRQn 1 */
+/* USER CODE END RTC_IRQn 1 */
 //}
 
 /**
-  * @brief This function handles EXTI line 0 and line 1 interrupts.
-  */
+ * @brief This function handles EXTI line 0 and line 1 interrupts.
+ */
 //void EXTI0_1_IRQHandler(void)
 //{
-  /* USER CODE BEGIN EXTI0_1_IRQn 0 */
+/* USER CODE BEGIN EXTI0_1_IRQn 0 */
 
-  /* USER CODE END EXTI0_1_IRQn 0 */
+/* USER CODE END EXTI0_1_IRQn 0 */
 //  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
 //  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_1);
-  /* USER CODE BEGIN EXTI0_1_IRQn 1 */
+/* USER CODE BEGIN EXTI0_1_IRQn 1 */
 
-  /* USER CODE END EXTI0_1_IRQn 1 */
+/* USER CODE END EXTI0_1_IRQn 1 */
 //}
 
 /**
-  * @brief This function handles EXTI line 2 and line 3 interrupts.
-  */
+ * @brief This function handles EXTI line 2 and line 3 interrupts.
+ */
 
 
 //void EXTI2_3_IRQHandler(void)
 //{
-  /* USER CODE BEGIN EXTI2_3_IRQn 0 */
+/* USER CODE BEGIN EXTI2_3_IRQn 0 */
 
-  /* USER CODE END EXTI2_3_IRQn 0 */
+/* USER CODE END EXTI2_3_IRQn 0 */
 // HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
-  /* USER CODE BEGIN EXTI2_3_IRQn 1 */
+/* USER CODE BEGIN EXTI2_3_IRQn 1 */
 
-  /* USER CODE END EXTI2_3_IRQn 1 */
+/* USER CODE END EXTI2_3_IRQn 1 */
 //}
 
 
 /**
-  * @brief This function handles EXTI line 4 to 15 interrupts.
-  */
+ * @brief This function handles EXTI line 4 to 15 interrupts.
+ */
 //void EXTI4_15_IRQHandler(void)
 //{
-  /* USER CODE BEGIN EXTI4_15_IRQn 0 */
+/* USER CODE BEGIN EXTI4_15_IRQn 0 */
 
-  /* USER CODE END EXTI4_15_IRQn 0 */
+/* USER CODE END EXTI4_15_IRQn 0 */
 //  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
 //  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
-  /* USER CODE BEGIN EXTI4_15_IRQn 1 */
+/* USER CODE BEGIN EXTI4_15_IRQn 1 */
 
-  /* USER CODE END EXTI4_15_IRQn 1 */
+/* USER CODE END EXTI4_15_IRQn 1 */
 //}
 
 /**
-  * @brief This function handles USART2 global interrupt / USART2 wake-up interrupt through EXTI line 26.
-  */
+ * @brief This function handles USART2 global interrupt / USART2 wake-up interrupt through EXTI line 26.
+ */
 void USART1_IRQHandler(void){
 
-  /* USER CODE BEGIN USART2_IRQn 0 */
-	if(__HAL_UART_GET_IT_SOURCE(&huart1, UART_IT_RXNE) != RESET)
-	 {
-		  printf("teste\r\n");
-	 }
-	 HAL_NVIC_ClearPendingIRQ(USART1_IRQn);
-//	return;
+#if 1
+	/* USER CODE BEGIN USART2_IRQn 0 */
+	if(__HAL_UART_GET_IT_SOURCE(&huart1, UART_IT_RXNE) != RESET){
+		printf("\r\ntesteu\r\n");
 
-  /* USER CODE END USART2_IRQn 0 */
+		//		uint8_t byte;
+		//
+		//		HAL_UART_Receive_IT(&huart1, (uint8_t *) byte, 1);
+		//
+		//		printf("\r\n%u\r\n", byte);
+
+	}
+	HAL_NVIC_ClearPendingIRQ(USART1_IRQn);
+	//	return;
+
+	/* USER CODE END USART2_IRQn 0 */
 	HAL_UART_IRQHandler(&huart1);
-  /* USER CODE BEGIN USART2_IRQn 1 */
+	/* USER CODE BEGIN USART2_IRQn 1 */
+#endif
+	/* USER CODE END USART2_IRQn 1 */
 
-  /* USER CODE END USART2_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
